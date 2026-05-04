6f99c722a4ff55a130fca26cba936a753e433b3a
matheustrabas <matheus.trabauco01102637@sesisenaipr.org.br>
2026-05-04 11:04:36 -0300

    verificação 26 css 16

--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
 function salvarCarros(event) {
     event.preventDefault();
     let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('title').value;
+    let marca = document.getElementById('title').value;
+    let modelo = document.getElementById('title').value;
 }
+
